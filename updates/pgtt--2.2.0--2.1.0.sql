4b24e4b50febbc5ce3bef9d96c3c7579d78b768d
Gilles Darold <gilles@darold.net>
2021-06-04 10:24:22 +0200

    Remove downgrade extension files, they are useless.

--- updates/pgtt--2.2.0--2.1.0.sql
+++
@@ -1,9 +0,0 @@
---  complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION pgtt" to load this file. \quit
-
--- check the functions bodies as creation time, enabled by default
-SET LOCAL check_function_bodies = on ;
-
--- make sure of client encoding
-SET LOCAL client_encoding = 'UTF8';
-
